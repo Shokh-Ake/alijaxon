0fd7cc039ccce35a554e576b810c683cb5944783
Shokh-Ake <shoxruxomonov1818@gmail.com>
2022-11-10 19:20:34 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,17 @@
+"""
+BU YERDA SIZGA KERAKLI BO'LADIGAN TOKEN VA SHUNGA O'XSHAGAN MA'LUMOTLAR SAQLANADI
+"""
+TOKEN = '5744091292:AAHvtdsqWRO1skJx-GRfzMu_ux2GoJNdCfM'
+
+PROVIDER_TOKEN = "371317599:TEST:1667912104203"
+ADMINS = '''Adminlarni saqlash uchun o'zgaruvchi, listdaham saqlasa bo'ladi'''
+
+
+
+DB_NAME = '''ddq209288clidm'''
+DB_HOST = '''ec2-34-242-84-130.eu-west-1.compute.amazonaws.com'''
+DB_PASSWORD = '''29c58e4607db4b40f7d7319bb9d507748a952ac5ba794e70d8e7e1315f99bfeb'''
+DB_PORT = '''5432'''
+DB_USER = '''haubaatpdzrtuj'''
+
+
